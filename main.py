d5f882fc9e64d122ee7e79fcc0a9c491ed1c0878
Jalil <agoumi.jalil@gmail.com>
2017-12-22 19:03:33 +0100

    maj

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from user_img_proc import face_detection
 from imageio import imread
 import numpy as n
+import matplotlib.pyplot as plt
 fd = face_detection()
-fd.get_faces(imread("img/f3.jpg"),True,True)
+# fd.train("test/")
+res,_ = fd.load_data_paths()
+res = fd.read_img(res)
+print(n.shape(res))
+for i in range(14):
+	plt.figure()
+	plt.imshow(res[i,:,:,:])
+plt.show()
+
+
+# fd.get_faces(imread("test/f3.jpg"),True,True)
 # x,y = fd.load_data("test")
 # yy = range(len(y))
 # fr = face_recognition()
--- user_img_proc.py
+++ user_img_proc.py
@@ -3,13 +3,15 @@ sys.path.insert(0,'../facenet/')
 import tensorflow as tf
 # import nn4 as network
 import os
-from src.align import detect_face,align_dataset_mtcnn
+from src.align import detect_face
+from src import facenet
 from imageio import imread
 import cv2
 import matplotlib.pyplot as plt
 import re
 import numpy as n
 import datetime
+import math 
 
 class face_detection():
 	#   setup facenet parameters
@@ -24,7 +26,7 @@ class face_detection():
 	random_state = 42
 	model_dir = "models/"
 	pre_trained_model_name = "20170511-185253"
-	pre_trained_model = self.model_dir+"pre_trained/"+self.pre_trained_model_name+"/"+self.pre_trained_model_name+".pb"
+	pre_trained_model = model_dir+"pre_trained/"+pre_trained_model_name+"/"+pre_trained_model_name+".pb"
 	batch_size = 1000
 
 	def __init__(self,image_size=96):
@@ -57,7 +59,7 @@ class face_detection():
 		if with_plot: plt.show()
 		return res
 
-	def load_data(self,fpath,reshape=False):
+	def load_data(self,fpath):
 		data = dict()
 		datax = list()
 		datay = list()
@@ -77,6 +79,33 @@ class face_detection():
 					datay.append(fid)
 		return datax,datay
 
+	def load_data_paths(self,img_path="test/"):
+		datax = list()
+		datay = list()
+		for fid in os.listdir(img_path):
+			res = list()
+			print(img_path)
+			for i in os.listdir(img_path+fid):
+				if re.search('.+.jpg',i):
+					datax.append(img_path+fid+"/"+i)
+					datay.append(fid)
+		return datax,datay
+
+
+	def read_img(self,fpath,reshape=False):
+		res = list()
+		for i in fpath:
+			print(i)
+			img=cv2.cvtColor(cv2.imread(i), cv2.COLOR_BGR2GRAY)
+			if img.ndim == 2: img = self.to_rgb(img)
+			img = self.get_faces(img)
+			if len(img) > 1: return None
+			img = cv2.resize(img[0],(self.image_size,self.image_size), interpolation=cv2.INTER_CUBIC)
+			if reshape: img =img.reshape(-1,self.image_size,self.image_size,3) 
+			res.append(img)
+		return n.array(res)
+
+
 	def split_args(self,sample_size,coef=0.7):
 		a = n.random.permutation(n.arange(sample_size))
 		x = int(coef*sample_size)
@@ -84,97 +113,88 @@ class face_detection():
 
 
 	def to_rgb(self,img):
-	    w, h = img.shape
-	    ret = n.empty((w, h, 3), dtype=n.uint8)
-	    ret[:, :, 0] = ret[:, :, 1] = ret[:, :, 2] = img
-	    return ret
+		w, h = img.shape
+		ret = n.empty((w, h, 3), dtype=n.uint8)
+		ret[:, :, 0] = ret[:, :, 1] = ret[:, :, 2] = img
+		return ret
 
-	def train(self,datax,datay,datalab):
-    with tf.Graph().as_default():
-        with tf.Session() as sess:
-            n.random.seed(seed=self.random_state)
-            # Load the model
-            print('Loading feature extraction model')
-            facenet.load_model(self.pre_trained_model)            
-            # Get input and output tensors
-            images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
-            embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
-            phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
-            embedding_size = embeddings.get_shape()[1]            
-            # Run forward pass to calculate embeddings
-            print('Calculating features for images')
-            nrof_images = len(datax)
-            nrof_batches_per_epoch = int(math.ceil(1.0*nrof_images / self.batch_size))
-            emb_array = np.zeros((nrof_images, embedding_size))
-            for i in range(nrof_batches_per_epoch):
-                start_index = i*self.batch_size
-                end_index = min((i+1)*self.batch_size, nrof_images)
-                # paths_batch = paths[start_index:end_index]
-                # images = facenet.load_data(paths_batch, False, False, args.image_size)
-                feed_dict = { images_placeholder:datax, phase_train_placeholder:False }
-                emb_array[start_index:end_index,:] = sess.run(embeddings, feed_dict=feed_dict)
-            
-            classifier_filename_exp = os.path.expanduser(args.classifier_filename)
+	def train(self,img_path="img/"):
+		datax,datay = self.load_data_paths(img_path)
+		imgs = self.read_img(datax,False)
+		with tf.Graph().as_default():
+			with tf.Session() as sess:
+				n.random.seed(seed=self.random_state)
+				# Load the model
+				print('Loading feature extraction model')
+				facenet.load_model(self.pre_trained_model)            
+				# Get input and output tensors
+				images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+				embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+				phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+				embedding_size = embeddings.get_shape()[1]            
+				# Run forward pass to calculate embeddings
+				print('Calculating features for images')
+				nrof_images = len(datax)
+				# nrof_batches_per_epoch = int(math.ceil(1.0*nrof_images / self.batch_size))
+				# emb_array = n.zeros((nrof_images, embedding_size))
+				# for i in range(nrof_batches_per_epoch):
+				# 	start_index = i*self.batch_size
+				# 	end_index = min((i+1)*self.batch_size, nrof_images)
+				# 	# paths_batch = paths[start_index:end_index]
+				# 	# images = facenet.load_data(paths_batch, False, False, args.image_size)
+				feed_dict = { images_placeholder:imgs, phase_train_placeholder:False }
+				emb_array = sess.run(embeddings, feed_dict=feed_dict)
+				
+				# classifier_filename_exp = os.path.expanduser(args.classifier_filename)
 
-            if (args.mode=='TRAIN'):
-                # Train classifier
-                print('Training classifier')
-                model = SVC(kernel='linear', probability=True)
-                model.fit(emb_array, datay)
-            
-                # Create a list of class names
-                # class_names = [ cls.name.replace('_', ' ') for cls in dataset]
+				print('Training classifier')
+				model = SVC(kernel='linear', probability=True)
+				model.fit(emb_array, n.arange(len(datay)))
 
-                # Saving classifier model
-                with open(classifier_filename_exp, 'wb') as outfile:
-                    pickle.dump((model, datalab), outfile)
-                print('Saved classifier model to file "%s"' % classifier_filename_exp)
-                
-            elif (args.mode=='CLASSIFY'):
-                # Classify images
-                print('Testing classifier')
-                model_name = datetime.datetime.now().strftime("%d%m%Y")+".model"
-                with open(model_dir+model_name, 'rb') as infile:
-                    (model, class_names) = pickle.load(infile)
+				# Saving classifier model
+				model_name = datetime.datetime.now().strftime("%d%m%Y")+".model"
+				with open(model_name, 'wb') as outfile:
+					pickle.dump((model, datay), outfile)
+				print('Saved classifier model to file "%s"' % classifier_filename_exp)
+					
+	def predict(self,fpath):
+		img = self.read_img([fpath])
+		with tf.Graph().as_default():
+			with tf.Session() as sess:
+				n.random.seed(seed=self.random_state)
+				# Load the model
+				print('Loading feature extraction model')
+				facenet.load_model(self.pre_trained_model)            
+				# Get input and output tensors
+				images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+				embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+				phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+				embedding_size = embeddings.get_shape()[1]            
+				# Run forward pass to calculate embeddings
+				print('Calculating features for images')
+				nrof_images = len(datax)
+				nrof_batches_per_epoch = int(math.ceil(1.0*nrof_images / self.batch_size))
+				emb_array = n.zeros((nrof_images, embedding_size))
+				for i in range(nrof_batches_per_epoch):
+					start_index = i*self.batch_size
+					end_index = min((i+1)*self.batch_size, nrof_images)
+					feed_dict = { images_placeholder:img, phase_train_placeholder:False }
+					emb_array[start_index:end_index,:] = sess.run(embeddings, feed_dict=feed_dict)
+					# Classify images
+					print('Testing classifier')
+				with open(model_dir+model_name, 'rb') as infile:
+					(model, class_names) = pickle.load(infile)
 
-                print('Loaded classifier model from file "%s"' % classifier_filename_exp)
-
-                predictions = model.predict_proba(emb_array)
-                best_class_indices = np.argmax(predictions, axis=1)
-                best_class_probabilities = predictions[np.arange(len(best_class_indices)), best_class_indices]
-                
-                for i in range(len(best_class_indices)):
-                    print('%4d  %s: %.3f' % (i, class_names[best_class_indices[i]], best_class_probabilities[i]))
-                    
-                accuracy = np.mean(np.equal(best_class_indices, labels))
-                print('Accuracy: %.3f' % accuracy)
-    def predict()
+				predictions = model.predict_proba(emb_array)
+				print(predictions)
+				# best_class_indices = np.argmax(predictions, axis=1)
+				# best_class_probabilities = predictions[np.arange(len(best_class_indices)), best_class_indices]
+				
+				# for i in range(len(best_class_indices)):
+				#     print('%4d  %s: %.3f' % (i, class_names[best_class_indices[i]], best_class_probabilities[i]))
+					
+				# accuracy = np.mean(np.equal(best_class_indices, labels))
+				# print('Accuracy: %.3f' % accuracy)
 
 
 
-# class face_recognition():
-# 	image_size = 96
-# 	model_dir='models/'
-# 	model_def = 'models.nn4'
-# 	pool_type = "Max" #{'MAX', 'L2'}
-# 	use_lrn=False #"Enables Local Response Normalization after the first layers of the inception network."
-# 	seed=42,# "Random seed."
-# 	batch_size= None # "Number of images to process in a batch."
-# 	embeddings = None
-
-# 	def __init__(self):
-# 		tf.Graph().as_default()
-# 		sess = tf.Session()
-# 		images_placeholder = tf.placeholder(tf.float32, shape=(self.batch_size,self.image_size,self.image_size, 3), name='input')
-# 		phase_train_placeholder = tf.placeholder(tf.bool, name='phase_train')
-# 		print(phase_train_placeholder)
-# 		self.embeddings = network.inference(images_placeholder, self.pool_type,self.use_lrn,1.0)
-# 		ema = tf.train.ExponentialMovingAverage(1.0)
-# 		saver = tf.train.Saver(ema.variables_to_restore())
-# 		saver.restore(sess, model_dir + 'model.ckpt')
-
-# 	def process_xvec(self,xdata):
-# 		res= list()
-# 		for i in xdata:
-# 			res.append(sess.run([self.embeddings],feed_dict={images_placeholder: np.array(i), phase_train_placeholder: False })[0])
-# 		return res
